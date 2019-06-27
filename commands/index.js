89361087bc4009d42655f8f42c7ab2b880417c8b
liuym123 <2777230076@qq.com>
2019-06-27 17:18:12 +0800

    'autocommit'

--- commands/index.js
+++ commands/index.js
@@ -1,15 +1,13 @@
 #! /usr/bin/env node
 
 const program = require('commander')
-const git = require('./auto-push')
-const automation = require('./auto-automation')
+const git = require('./git')
 
 program
     .version('1.0.0', '-v, --version')
     .option('-p, --push', 'autopush 直接推送文件')
     .option('-l, --pull', 'pull 拉取文件，然后推送')
     .option('-m, --pushm [ops]', 'push 文件接 描述信息')
-    .option('-a, --auto', '自动化模板构建页面配置与主页归档')
 
 
 program.on('--help', () => {
@@ -29,7 +27,4 @@ if (program.pull) {
 }
 if (program.pushm) {
     git('m', program.pushm)
-}
-if (program.auto) {
-    automation('a')
 }
