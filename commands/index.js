8353b3a1f9f5bcf1c2d04ff6c79c47432ff8b204
mrzhoulichao <1751777388@qq.com>
2019-06-29 00:30:20 +0800

    'autocommit'

--- commands/index.js
+++ commands/index.js
@@ -7,14 +7,12 @@ program
     .version('1.0.0', '-v, --version')
     .option('-p, --push', 'autopush 直接推送文件')
     .option('-l, --pull', 'pull 拉取文件，然后推送')
-    .option('-m, --pushm [ops]', 'push 文件接 描述信息')
-
+    .option('-m, --pushm [ops]', 'push 文件接 描述信息,使用双引号描述')
 
 program.on('--help', () => {
-    console.log('                  ')
-    console.log(' **************** ')
-    console.log('       测试        ')
-    console.log('                  ')
+    console.log('                                                                   ')
+    console.log(' ***************************************************************** ')
+    console.log('     [fllow me](https://mrzhoulichao.github.io/reco/)              ')
 })
 
 program.parse(process.argv);
