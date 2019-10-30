7fe8fb54f0efb9edeb874c8b52ad97746c6acda1
jojo-f <jojo@dcpt.online>
2019-10-30 18:38:43 +0800

    'autocommit'

--- commands/index.js
+++ commands/index.js
@@ -1,35 +1,44 @@
 #! /usr/bin/env node
 
 const program = require('commander')
-const shell  = require('shelljs')
+const shell = require('shelljs')
 const git = require('./git')
 
 program
     .version('1.0.0', '-v, --version')
     .option('-p, --push', 'autopush 直接推送文件')
     .option('-l, --pull', 'pull 拉取文件，然后推送')
-    .option('-m, --pushm [ops]', 'push 文件接 描述信息,使用双引号描述')
-    .option("-o, --orz","用于orz.io博客打包")
+    .option('-m, --pushm [msg]', 'push 文件接 描述信息,使用双引号描述')
+    .option("-o, --orz [msg]", "用于orz.io博客打包")
+    .option("-c, --copy [origin] [target]", "拷贝origin至target")
 
 program.on('--help', () => {
     console.log('                                                                   ')
     console.log(' ***************************************************************** ')
-    console.log('     [fllow me](https://mrzhoulichao.github.io/reco/)              ')
+    console.log('     [fllow me](https://llorz-o.github.io/)              ')
 })
 
 program.parse(process.argv);
 
 if (program.push) {
     git('p')
-}else
+} else
 if (program.pull) {
     git('l')
-}else
+} else
 if (program.pushm) {
     git('m', program.pushm)
-}else
-if(program.orz){
+} else
+if (program.orz) {
     shell.exec("npm run build")
+    shell.exec("git add .")
+    shell.exec("git commit -m 'build'")
+    shell.exec("git push")
     shell.exec("git checkout master")
-    shell.cp('-R', '../temp', '/');
+    shell.cp('-R', '../temp/*', './');
+    shell.exec("git add .")
+    shell.exec(`git commit -m '${program.orz}'`)
+}
+if (program.copy) {
+    shell.cp('-R', '../temp/*', './');
 }
