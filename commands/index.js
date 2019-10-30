a2e4c7f6528264a6886de8bc8c5b7300a674737a
jojo-f <jojo@dcpt.online>
2019-10-30 16:49:27 +0800

    'autocommit'

--- commands/index.js
+++ commands/index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
 const program = require('commander')
+const shell  = require('shelljs')
 const git = require('./git')
 
 program
@@ -8,6 +9,7 @@ program
     .option('-p, --push', 'autopush 直接推送文件')
     .option('-l, --pull', 'pull 拉取文件，然后推送')
     .option('-m, --pushm [ops]', 'push 文件接 描述信息,使用双引号描述')
+    .option("-o, --orz","用于orz.io博客打包")
 
 program.on('--help', () => {
     console.log('                                                                   ')
@@ -19,10 +21,15 @@ program.parse(process.argv);
 
 if (program.push) {
     git('p')
-}
+}else
 if (program.pull) {
     git('l')
-}
+}else
 if (program.pushm) {
     git('m', program.pushm)
+}else
+if(program.orz){
+    shell.exec("npm run build")
+    shell.exec("git checkout master")
+    shell.cp('-R', '../temp', '/');
 }
